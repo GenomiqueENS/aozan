2ab513039353f9bedcc790494d3487ee924859f5
jourdren <jourdren@frigg.ens.fr>
2011-12-16 10:17:06 +0100

    TYPO: In hiseq_run.py fix in mail the disk usage and disk free space.

--- src/main/python/hiseq_run.py
+++ src/main/python/hiseq_run.py
@@ -199,4 +199,4 @@ def send_mail_if_recent_run(run_id, secs, conf):
 		common.send_msg('[Aozan] End of the HiSeq run ' + run_id, 'A new run (' + run_id + ') has been terminated at ' +
 					common.time_to_human_readable(last) + '.\n' +
 					'Data for this run can be found at: ' + run_path +
-					'\n\n%.2f Gb has been used,' % du + ' %.2f Gb still free.' % df, conf)
+					'\n\nFor this task %.2f GB has been used and ' % du + ' %.2f GB still free.' % df, conf)
