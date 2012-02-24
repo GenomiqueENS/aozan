5e328a6f2147a0f8bcb446b4b2134b5c05001fb0
jourdren <jourdren@5a62d92b-e016-0410-9103-b89fe532bb34>
2012-02-24 19:16:12 +0000

    UPDATE: In demux step, add Casava design warning to log and in the success mail send to users.
    git-svn-id: https://subversion.biologie.ens.fr/svn/leburon/trunk/aozan@2192 5a62d92b-e016-0410-9103-b89fe532bb34

--- src/main/python/demux_run.py
+++ src/main/python/demux_run.py
@@ -181,7 +181,7 @@ def demux(run_id, conf):
         CasavaDesignUtil.replaceIndexShortcutsBySequences(design, load_index_sequences(conf))
         
         # Check values of design file
-        CasavaDesignUtil.checkCasavaDesign(design, flow_cell_id)
+        design_warnings = CasavaDesignUtil.checkCasavaDesign(design, flow_cell_id)
         
         # Write CSV design file
         CasavaDesignCSVWriter(design_csv_path).writer(design)
@@ -192,6 +192,18 @@ def demux(run_id, conf):
     except EoulsanException, exp:
         error("error while converting design-%04d" % run_number + ".xls to CSV format", exp.getMessage(), conf)
         return False
+
+    # Log Casava design warning
+    if (design_warnings>0):
+        msg = ''
+        first = True
+        for warn in design_warnings:
+            if first:
+                first = False
+            else:
+                msg += ' '
+            msg += warn
+        common.log("INFO", "casava design warnings: " + msg, conf)
 
     # Create casava makefile
     cmd = conf['casava.path'] + '/bin/configureBclToFastq.pl ' + \
@@ -276,6 +288,11 @@ def demux(run_id, conf):
         ' with no error in ' + common.duration_to_human_readable(duration) + '.\n\n' + \
         'Fastq files for this run ' + \
         'can be found in the following directory:\n  ' + fastq_output_dir
+
+    if design_warnings.size()>0:
+        msg+='\n\nDesign warnings:'
+        for warn in design_warnings:
+            msg+="\n  - " + warn 
 
     # Add path to report if reports.url exists
     if conf['reports.url'] != None and conf['reports.url'] != '':
