ec36ca16f8600f54d29510e557ed74c1a7c59477
Sandrine Perrin <sperrin@biologie.ens.fr>
2012-11-23 17:54:45 +0100

    ADD : execute fastqscreen, in local, with pseudoMapReduce and filter bowtie output SAM with the readAlignmentFilter from Eoulsan

--- src/main/java/fr/ens/transcriptome/aozan/fastqscreen/FastqScreen.java
+++ src/main/java/fr/ens/transcriptome/aozan/fastqscreen/FastqScreen.java
@@ -22,9 +22,13 @@ import fr.ens.transcriptome.eoulsan.bio.FastqFormat;
 import fr.ens.transcriptome.eoulsan.bio.readsmappers.AbstractBowtieReadsMapper;
 import fr.ens.transcriptome.eoulsan.bio.readsmappers.Bowtie2ReadsMapper;
 import fr.ens.transcriptome.eoulsan.bio.readsmappers.BowtieReadsMapper;
+import fr.ens.transcriptome.eoulsan.bio.readsmappers.GSNAPReadsMapper;
 import fr.ens.transcriptome.eoulsan.bio.readsmappers.SequenceReadsMapper;
+import fr.ens.transcriptome.eoulsan.steps.AbstractStep;
+import fr.ens.transcriptome.eoulsan.steps.mapping.GsnapStep;
 import fr.ens.transcriptome.eoulsan.util.Reporter;
 import fr.ens.transcriptome.eoulsan.util.StringUtils;
+import fr.ens.transcriptome.aozan.fastqscreen.FastsqScreenSAMParser;
 
 public class FastqScreen {
 
@@ -39,12 +43,10 @@ public class FastqScreen {
   // file path bowtie (local)
   final static String bowtiePath = "/home/sperrin/Programmes/bowtie/bowtie";
 
-  final static File fastqFileZip = new File(
-      "/home/sperrin/Documents/FastqScreenTest/extrait.fastq.gz");
-
   static File fastqFile = new File(
       "/home/sperrin/Documents/FastqScreenTest/extrait.txt");
 
+  
   final static File indexFileAdapters_method2 =
       new File(
           "/home/sperrin/shares-net/ressources/sequencages/fastq_screen/Adapters/Adapters.1.ebwt");
@@ -57,7 +59,14 @@ public class FastqScreen {
           "/home/sperrin/shares-net/ressources/sequencages/fastq_screen/Adapters/Adapters");
   final static File indexFilePhix_method1 = new File(
       "/home/sperrin/shares-net/ressources/sequencages/fastq_screen/PhiX/PhiX");
-
+  final static File indexDirPhix_method1 = new File(
+          "/home/sperrin/shares-net/ressources/sequencages/fastq_screen/PhiX");
+  final static File genomeDescriptionPhix =  
+		  new File ("/home/sperrin/shares-net/ressources/sequencages/mappers_indexes/bowtie-eaa26a56aeaccd77c44ea4168b6f5cdb.zip");
+  
+  final static File indexDirAdapters_method1 = new File(
+          "/home/sperrin/shares-net/ressources/sequencages/fastq_screen/Adapters");
+          
   static File outputFile = null;
   static List<File> listOutputFile = new ArrayList<File>();
 
@@ -71,137 +80,24 @@ public class FastqScreen {
 
     EoulsanRuntimeDebug.initDebugEoulsanRuntime();
     // eoulsan runtime exception method2();
-    method2();
-
+    
+    method3();
+    
   }
+  
+  static void method3() throws IOException, InterruptedException {
 
-  // launch fastq screen with bowtieReadsMapper
-  static void method2() throws IOException, InterruptedException {
-
-    AbstractBowtieReadsMapper bowtie = new BowtieReadsMapper();
-    Reporter reporter = new Reporter();
-
-    final File[] listArchiveIndexFile =
-        {indexFilePhix_method2, indexFileAdapters_method2};
-    final File[] listArchiveIndexDir =
-        {
-            new File(
-                "/home/sperrin/shares-net/ressources/sequencages/fastq_screen/PhiX"),
-            new File(
-                "/home/sperrin/shares-net/ressources/sequencages/fastq_screen/Adapters")};
-
-    mapSingleEnd(FastqFormat.FASTQ_SANGER, bowtie, fastqFile,
-        listArchiveIndexFile, listArchiveIndexDir, reporter);
-  }
-
-  // change : use table of file instead of one file
-  // method from ReadsMapperLocalStep
-  private static void mapSingleEnd(FastqFormat fastqFormat,
-      final SequenceReadsMapper bowtie, final File inFile,
-      final File[] archiveIndexFile, final File[] indexDir,
-      final Reporter reporter) throws IOException {
-
-    // Init mapper
-
-    // Set mapper arguments
-    /*
-     * final int mapperThreads = initMapperArguments(mapper,
-     * context.getSettings() .getTempDirectoryFile());
-     */
-    final int mapperThreads = 1;
-
-    // Mapper change defaults arguments
-    final int seed = 40;
-
-    final String newArgumentsMapper = " -l " + seed + " --chunkmbs 512";
-
-    // " -l " + seed + " -k 2 --chunkmbs 512 -p 2 -S";
-
-    // LOGGER
-    // .info("Map file: "
-    // + inFile + ", Fastq format: " + fastqFormat + ", use "
-    // + bowtie.getMapperName() + " with " + mapperThreads
-    // + " threads option");
-
-    // loop : Process to mapping for each library
-    for (int i = 0; i < indexDir.length; i++) {
-      bowtie.init(false, fastqFormat, archiveIndexFile[i], indexDir[i],
-          reporter, COUNTER_GROUP);
-      bowtie.setMapperArguments(newArgumentsMapper);
-      bowtie.setThreadsNumber(mapperThreads);
-
-      bowtie.map(inFile);
-
-    }
-    /*
-     * listOutputFile.add(bowtie.getSAMFile(null)); for (File f :
-     * listOutputFile) System.out.println("SAM de bowtie " + f.getName());
-     */
-  }
-
-  static void method1() throws IOException, InterruptedException {
-
-    List<File> listGenome = new ArrayList<File>();
-    listGenome.add(indexFilePhix_method1);
-    listGenome.add(indexFileAdapters_method1);
-
-    // fastqFile = new File("/home/sperrin/Documents/FastqScreenTest");
-    // FileUtils.unzip(fastqFileZip, fastqFile);
+    List<File[]> listGenome = new ArrayList<File[]>();
+    listGenome.add(new File[]{indexFilePhix_method2, indexDirPhix_method1});
+    listGenome.add(new File[]{indexFileAdapters_method2,indexDirAdapters_method1});
 
     FastqScreenPseudoMapReduce pmr =
         new FastqScreenPseudoMapReduce(readsprocessed);
+    
+    pmr.setMapReduceTemporaryDirectory(new File("/home/sperrin"));
+    pmr.doMap(fastqFile, listGenome);
 
-    ProcessBuilder pb;
-    Process p = null;
-
-    for (File index : listGenome) {
-
-      // use option -S to generate SAM output format
-      System.out.println("name index : " + index.toString());
-
-      String paramFileFastq =
-          StringUtils.bashEscaping(fastqFile.getAbsolutePath());
-
-      List<String> cmd = new ArrayList<String>();
-      cmd.add(bowtiePath);
-      cmd.add("-l");
-      cmd.add("40");
-      cmd.add("-k");
-      cmd.add("2");
-      cmd.add("--chunkmbs");
-      cmd.add("512");
-      cmd.add("-p");
-      cmd.add("2");
-      cmd.add(index.getAbsolutePath());
-      cmd.add(paramFileFastq);
-      cmd.add("-S");
-
-      pb = new ProcessBuilder(cmd);
-
-      // ProcessBuilder pb =
-      // new ProcessBuilder(bowtiePath, "--phred33", "-p", "2", "-x", "genome",
-      // "-", "2>", "/dev/null");
-
-      // pb =
-      // new ProcessBuilder(bowtiePath, "-l", "40", "-k", "2", "--chunkmbs",
-      // "512", "-p", "2", index.toString(),
-      // fastqFile.getAbsolutePath(), "-S");
-
-      System.out.println("Command bowtie " + cmd.toString().replace(',', ' '));
-      // LOGGER.info("Command bowtie " + cmd);
-
-      // pb.directory(e.getValue());
-
-      pb.redirectErrorStream();
-      p = pb.start();
-
-      pmr.setMapReduceTemporaryDirectory(new File("/home/sperrin"));
-      pmr.setGenomeReference(index.getName().toString());
-      pmr.doMap(p.getInputStream());
-
-    } // loop for listGenome
-
-    pmr.doReduce(p.getOutputStream());
+    pmr.doReduce(new File("/home/sperrin/outputDoReduce.txt"));
     pmr.getStatisticalTable();
 
     final long endTime = System.currentTimeMillis();
@@ -221,6 +117,72 @@ public class FastqScreen {
      */
   }
 
+//launch fastq screen with bowtieReadsMapper
+ static void method2() throws IOException, InterruptedException {
+
+   AbstractBowtieReadsMapper bowtie = new BowtieReadsMapper();
+   Reporter reporter = new Reporter();
+
+   final File[] listArchiveIndexFile =
+       {indexFilePhix_method2, indexFileAdapters_method2};
+   final File[] listArchiveIndexDir =
+       {
+           new File(
+               "/home/sperrin/shares-net/ressources/sequencages/fastq_screen/PhiX"),
+           new File(
+               "/home/sperrin/shares-net/ressources/sequencages/fastq_screen/Adapters")};
+
+   mapSingleEnd(FastqFormat.FASTQ_SANGER, bowtie, fastqFile,
+       listArchiveIndexFile, listArchiveIndexDir, reporter);
+ }
+
+ // change : use table of file instead of one file
+ // method from ReadsMapperLocalStep
+ private static void mapSingleEnd(FastqFormat fastqFormat,
+     final SequenceReadsMapper bowtie, final File inFile,
+     final File[] archiveIndexFile, final File[] indexDir,
+     final Reporter reporter) throws IOException {
+
+   // Init mapper
+
+   // Set mapper arguments
+   /*
+    * final int mapperThreads = initMapperArguments(mapper,
+    * context.getSettings() .getTempDirectoryFile());
+    */
+   final int mapperThreads = 1;
+
+   // Mapper change defaults arguments
+   final int seed = 40;
+
+   final String newArgumentsMapper = "-l " + seed + " --chunkmbs 512 ";
+
+   // " -l " + seed + " -k 2 --chunkmbs 512 -p 2 -S";
+
+   // LOGGER
+   // .info("Map file: "
+   // + inFile + ", Fastq format: " + fastqFormat + ", use "
+   // + bowtie.getMapperName() + " with " + mapperThreads
+   // + " threads option");
+
+   // loop : Process to mapping for each library
+   for (int i = 0; i < indexDir.length; i++) {
+     bowtie.init(false, fastqFormat, archiveIndexFile[i], indexDir[i],
+         reporter, COUNTER_GROUP);
+     bowtie.setMapperArguments(newArgumentsMapper);
+     
+     bowtie.setThreadsNumber(mapperThreads);
+
+     bowtie.map(inFile /*ParseLine()*/);
+
+   }
+   
+   
+//   listOutputFile.add(bowtie.getSAMFile(null));
+//   for (File f : listOutputFile) 
+//	   System.out.println("SAM de bowtie " + f.getName());
+   
+ }
   /**
    * TODO : value probably exist in data in RunData
    * @return number of read in fastq file
