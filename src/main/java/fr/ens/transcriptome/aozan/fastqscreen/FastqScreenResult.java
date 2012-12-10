c83371a3cd525ea306e17354fd8f7605209b91c1
Sandrine Perrin <sperrin@biologie.ens.fr>
2012-12-10 18:32:50 +0100

    FIX FastqScreenResult and FastqScreenPseudoMapReduce, removal of miscalculations

--- src/main/java/fr/ens/transcriptome/aozan/fastqscreen/FastqScreenResult.java
+++ src/main/java/fr/ens/transcriptome/aozan/fastqscreen/FastqScreenResult.java
@@ -12,6 +12,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.math.RoundingMode;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -28,7 +29,7 @@ public class FastqScreenResult {
   private final String[] LEGEND_RUNDATA = {"unmapped", "one.hit.one.library",
       "multiple.hits.one.library", "one.hit.multiple.libraries",
       "multiple.hits.multiple.libraries"};
-  private final String FINAL_LINE_RUNDATA = "Hit.no.libraries";
+  private final String FINAL_LINE_RUNDATA = "hit.no.libraries";
 
   // legend for file result
   private final String HEADER_COLUMNS_TEXT =
@@ -43,77 +44,6 @@ public class FastqScreenResult {
   private int readsprocessed;
   private int readsMapped;
 
-  
-  /**
-   * Called by method reduce for each read mapped and filled intermediate table
-   * @param genome
-   * @param oneHit
-   * @param oneGenome
-   */
-  void countHitPerGenome(String genome, boolean oneHit, boolean oneGenome) {
-    // indices for table tabHitsPerLibraries
-    // position 0 of the table for UNMAPPED ;
-
-    // System.out.println("genome : "
-    // + genome + " hit " + oneHit + " gen " + oneGenome);
-
-    final int ONE_HIT_ONE_LIBRARY = 1;
-    final int MULTIPLE_HITS_ONE_LIBRARY = 2;
-    final int ONE_HIT_MULTIPLE_LIBRARIES = 3;
-    final int MUTILPLE_HITS_MULTIPLE_LIBRARIES = 4;
-    float[] tab;
-    // genome must be contained in map
-    if (!(resultsPerGenome.containsKey(genome)))
-      return;
-
-    if (oneHit && oneGenome) {
-      tab = resultsPerGenome.get(genome);
-      tab[ONE_HIT_ONE_LIBRARY] += 1.0;
-
-    } else if (!oneHit && oneGenome) {
-      tab = resultsPerGenome.get(genome);
-      tab[MULTIPLE_HITS_ONE_LIBRARY] += 1.0;
-
-    } else if (oneHit && !oneGenome) {
-      tab = resultsPerGenome.get(genome);
-      tab[ONE_HIT_MULTIPLE_LIBRARIES] += 1.0;
-
-    } else if (!oneHit && !oneGenome) {
-      tab = resultsPerGenome.get(genome);
-      tab[MUTILPLE_HITS_MULTIPLE_LIBRARIES] += 1.0;
-    }
-  }// countHitPerGenome
-
-  
-  /**
-   * compute percent for each count of hits per reference genome without rounding 
-   * @return FastqScreenResult result of FastqScreen 
-   */
-  public FastqScreenResult getFastqScreenResult() {
-    
-    System.out.println("nb read mapped "
-        + readsMapped + " / nb read " + readsprocessed);
-
-    if (readsMapped > readsprocessed)
-      return null;
-
-    for (Map.Entry<String, float[]> e : resultsPerGenome.entrySet()) {
-      float unmapped = 100.f;
-      float[] tab = e.getValue();
-
-      for (int i = 1; i < tab.length; i++) {
-        float n = tab[i] * 100.f / readsprocessed;
-        tab[i] = n;
-        unmapped -= n;
-        // System.out.println("genome "
-        // + e.getKey() + " i : " + i + " val : " + n + " unmap " +
-        // unmapped);
-      }
-      tab[0] = unmapped;
-    }
-    return this;
-  }
-
   /**
    * print table percent in format use by fastqscreen program with rounding
    * @return
@@ -127,8 +57,6 @@ public class FastqScreenResult {
       s.append("\n" + e.getKey());
 
       for (float n : tab) {
-        // n = ((int) (n * 100.0)) / 100.0;
-        // n = Math.ceil(n);
         n = DoubleMath.roundToInt((n * 100.f), RoundingMode.HALF_DOWN) / 100.f;
         s.append("\t" + n);
       }
@@ -197,18 +125,18 @@ public class FastqScreenResult {
   public float getReadsprocessed() {
     return this.readsprocessed;
   }
-  
+
   public float getPercentHitNoLibraries() {
     return this.percentHitNoLibraries;
   }
-  
+
   //
   // SETTER
   //
 
   public void setPercentHitNoLibraries() {
     this.percentHitNoLibraries =
-        (readsprocessed - readsMapped) / readsprocessed * 100.f;
+        ((float) (readsprocessed - readsMapped)) / readsprocessed * 100.f;
   }
 
   public void setPaired(boolean paired) {
@@ -223,8 +151,6 @@ public class FastqScreenResult {
   // CONSTRUCTOR
   //
 
-  public FastqScreenResult(){}
-  
   public FastqScreenResult(Map<String, float[]> result, int readsMapped,
       int readsprocessed) {
     this.resultsPerGenome = result;
