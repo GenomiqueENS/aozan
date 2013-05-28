c26613f05a70533544ef2632f9807abacc4d0c49
Sandrine Perrin <sperrin@biologie.ens.fr>
2013-05-28 16:06:23 +0200

    ADD: ReadCollector calls ReadCollectorXML if read.xml file exists else ReadCollectorBinaryFile.

--- src/main/java/fr/ens/transcriptome/aozan/collectors/ReadCollector.java
+++ src/main/java/fr/ens/transcriptome/aozan/collectors/ReadCollector.java
@@ -64,7 +64,7 @@ public class ReadCollector implements Collector {
   /** The collector name. */
   public static final String COLLECTOR_NAME = "read";
 
-  private String RTAOutputDirPath;
+  protected static String RTAOutputDirPath;
 
   @Override
   public String getName() {
@@ -84,204 +84,239 @@ public class ReadCollector implements Collector {
     if (properties == null)
       return;
 
-    this.RTAOutputDirPath = properties.getProperty(QC.RTA_OUTPUT_DIR);
+    RTAOutputDirPath = properties.getProperty(QC.RTA_OUTPUT_DIR);
   }
 
   @Override
   public void collect(final RunData data) throws AozanException {
 
-    // Collect read info
-    final int readCount = data.getInt("run.info.read.count");
-
-    if (new File(this.RTAOutputDirPath + "/Data/reports/Summary", "read10.xml")
+    if (new File(RTAOutputDirPath + "/Data/reports/Summary", "read1.xml")
         .exists()) {
-
-      for (int i = 1; i <= readCount; i++) {
-        File readInfoFile =
-            new File(this.RTAOutputDirPath + "/Data/reports/Summary", "read"
-                + i + ".xml");
-        collectRead(data, readInfoFile);
-      }
+      new ReadCollectorXML(data);
     } else {
 
       // File read1.xml doesn't exist, reading binary files in InterOp directory
-      collectRead(data);
+      new ReadCollectorBinaryFile(data);
     }
-
-  }
-
-  private void collectRead(final RunData data) throws AozanException {
-    // Collect
-    System.out.println("read interop file");
-
-    AbstractBinaryIteratorReader.setDirectory(this.RTAOutputDirPath
-        + "/InterOp/");
-    new TileMetricsOutReader().collect(data);
-    new ErrorMetricsOutReader().collect(data);
-    new ExtractionMetricsOutReader().collect(data);
-  }
-
-  /**
-   * Collect data for a read?.xml file
-   * @param data RunData object
-   * @param readInfoFile the read?.xml file
-   * @throws AozanException if an error occurs while collecting data
-   */
-  private void collectRead(final RunData data, final File readInfoFile)
-      throws AozanException {
-
-    if (data == null)
-      return;
-
-    try {
-
-      // Create the input stream
-      final InputStream is = new FileInputStream(readInfoFile);
-
-      // Read the XML file
-      final DocumentBuilder dBuilder =
-          DocumentBuilderFactory.newInstance().newDocumentBuilder();
-      final Document doc = dBuilder.parse(is);
-      doc.getDocumentElement().normalize();
-
-      // Parse the document
-      parse(doc, data);
-
-      is.close();
-    } catch (IOException e) {
-
-      throw new AozanException(e);
-
-    } catch (SAXException e) {
-
-      throw new AozanException(e);
-    } catch (ParserConfigurationException e) {
-
-      throw new AozanException(e);
-    }
-  }
-
-  /**
-   * Parse the XML document.
-   * @param document Document DOM object
-   * @param data the result
-   */
-  private void parse(final Document document, final RunData data) {
-
-    for (Element e : XMLUtils.getElementsByTagName(document, "Summary")) {
-
-      int readNumber = -1;
-      double densityRatio = -1.0;
-      String readType = "";
-
-      // Parse Summary tag attributes
-      for (String attributeName : getAttributeNames(e)) {
-
-        if ("Read".equals(attributeName))
-          readNumber = Integer.parseInt(getAttributeValue(e, attributeName));
-        else if ("ReadType".equals(attributeName))
-          readType = getAttributeValue(e, attributeName);
-        else if ("densityRatio".equals(attributeName))
-          densityRatio =
-              Double.parseDouble(getAttributeValue(e, attributeName));
-      }
-
-      final String prefix = "read" + readNumber;
-      data.put(prefix + ".density.ratio", densityRatio);
-      data.put(prefix + ".type", readType);
-
-      // Parse Lane tag
-      for (Element laneElement : XMLUtils.getElementsByTagName(e, "Lane")) {
-
-        int lane = -1;
-        Map<String, String> map = Maps.newLinkedHashMap();
-
-        for (String key : XMLUtils.getAttributeNames(laneElement)) {
-
-          final String value = laneElement.getAttribute(key);
-
-          if ("key".equals(key))
-            lane = Integer.parseInt(value);
-          else
-            map.put(convertKey(key), value);
-        }
-
-        for (Map.Entry<String, String> entry : map.entrySet())
-          data.put(prefix + ".lane" + lane + "." + entry.getKey(),
-              entry.getValue());
-      }
-    }
-  }
-
-  /**
-   * Convert automatically attribute names to keys in RunData keys format.
-   * @param key key to convert
-   * @return the converted key
-   */
-  private static String convertKey(final String key) {
-
-    if (key == null)
-      return null;
-
-    if (key.length() == 1)
-      return key.toLowerCase();
-
-    String k = key;
-    if (k.endsWith("SD"))
-      k = k.substring(0, k.length() - 2) + ".sd";
-
-    k = k.replace("PhiX", ".phix");
-
-    final StringBuilder sb = new StringBuilder();
-    final int len = k.length();
-
-    int lastUp = 0;
-
-    boolean lastDigit = false;
-    int startWord = 0;
-
-    for (int i = 1; i < len; i++) {
-
-      final char c = k.charAt(i);
-      final boolean up = Character.isUpperCase(c);
-      final boolean digit = Character.isDigit(c);
-
-      if (up && lastUp > 0) {
-        lastUp++;
-        continue;
-      }
-
-      if (digit && lastDigit)
-        continue;
-
-      if (lastUp > 0 && !up) {
-
-        if (i > 1 && lastUp > 2) {
-          sb.append(k.substring(startWord, i - 1));
-          sb.append('.');
-          startWord = i - 1;
-        }
-
-        lastUp = 0;
-        continue;
-      }
-
-      if (up || digit) {
-        sb.append(k.substring(startWord, i));
-        sb.append('.');
-        startWord = i;
-
-        lastUp = up ? 1 : 0;
-        lastDigit = digit;
-      }
-
-    }
-    sb.append(k.substring(startWord));
-
-    return sb.toString();
   }
 
   @Override
   public void clear() {
+  }
+
+  //
+  // Internal class
+  //
+
+  /**
+   * This class define a Collector for read?.xml files.
+   * @since 1.0
+   * @author Laurent Jourdren
+   */
+  class ReadCollectorXML extends ReadCollector {
+
+    /**
+     * Collect data for a read?.xml file
+     * @param data RunData object
+     * @param readInfoFilePath the read?.xml file path
+     * @throws AozanException if an error occurs while collecting data
+     */
+    private void collectRead(final RunData data, final String readInfoFilePath)
+        throws AozanException {
+
+      if (data == null)
+        return;
+
+      try {
+
+        // Create the input stream
+        final InputStream is = new FileInputStream(new File(readInfoFilePath));
+
+        // Read the XML file
+        final DocumentBuilder dBuilder =
+            DocumentBuilderFactory.newInstance().newDocumentBuilder();
+        final Document doc = dBuilder.parse(is);
+        doc.getDocumentElement().normalize();
+
+        // Parse the document
+        parse(doc, data);
+
+        is.close();
+      } catch (IOException e) {
+
+        throw new AozanException(e);
+
+      } catch (SAXException e) {
+
+        throw new AozanException(e);
+      } catch (ParserConfigurationException e) {
+
+        throw new AozanException(e);
+      }
+    }
+
+    /**
+     * Parse the XML document.
+     * @param document Document DOM object
+     * @param data the result
+     */
+    private void parse(final Document document, final RunData data) {
+
+      for (Element e : XMLUtils.getElementsByTagName(document, "Summary")) {
+
+        int readNumber = -1;
+        double densityRatio = -1.0;
+        String readType = "";
+
+        // Parse Summary tag attributes
+        for (String attributeName : getAttributeNames(e)) {
+
+          if ("Read".equals(attributeName))
+            readNumber = Integer.parseInt(getAttributeValue(e, attributeName));
+          else if ("ReadType".equals(attributeName))
+            readType = getAttributeValue(e, attributeName);
+          else if ("densityRatio".equals(attributeName))
+            densityRatio =
+                Double.parseDouble(getAttributeValue(e, attributeName));
+        }
+
+        final String prefix = "read" + readNumber;
+        data.put(prefix + ".density.ratio", densityRatio);
+        data.put(prefix + ".type", readType);
+
+        // Parse Lane tag
+        for (Element laneElement : XMLUtils.getElementsByTagName(e, "Lane")) {
+
+          int lane = -1;
+          Map<String, String> map = Maps.newLinkedHashMap();
+
+          for (String key : XMLUtils.getAttributeNames(laneElement)) {
+
+            final String value = laneElement.getAttribute(key);
+
+            if ("key".equals(key))
+              lane = Integer.parseInt(value);
+            else
+              map.put(convertKey(key), value);
+          }
+
+          for (Map.Entry<String, String> entry : map.entrySet())
+            data.put(prefix + ".lane" + lane + "." + entry.getKey(),
+                entry.getValue());
+        }
+      }
+    }
+
+    /**
+     * Convert automatically attribute names to keys in RunData keys format.
+     * @param key key to convert
+     * @return the converted key
+     */
+    private String convertKey(final String key) {
+
+      if (key == null)
+        return null;
+
+      if (key.length() == 1)
+        return key.toLowerCase();
+
+      String k = key;
+      if (k.endsWith("SD"))
+        k = k.substring(0, k.length() - 2) + ".sd";
+
+      k = k.replace("PhiX", ".phix");
+
+      final StringBuilder sb = new StringBuilder();
+      final int len = k.length();
+
+      int lastUp = 0;
+
+      boolean lastDigit = false;
+      int startWord = 0;
+
+      for (int i = 1; i < len; i++) {
+
+        final char c = k.charAt(i);
+        final boolean up = Character.isUpperCase(c);
+        final boolean digit = Character.isDigit(c);
+
+        if (up && lastUp > 0) {
+          lastUp++;
+          continue;
+        }
+
+        if (digit && lastDigit)
+          continue;
+
+        if (lastUp > 0 && !up) {
+
+          if (i > 1 && lastUp > 2) {
+            sb.append(k.substring(startWord, i - 1));
+            sb.append('.');
+            startWord = i - 1;
+          }
+
+          lastUp = 0;
+          continue;
+        }
+
+        if (up || digit) {
+          sb.append(k.substring(startWord, i));
+          sb.append('.');
+          startWord = i;
+
+          lastUp = up ? 1 : 0;
+          lastDigit = digit;
+        }
+
+      }
+      sb.append(k.substring(startWord));
+
+      return sb.toString();
+    }
+
+    ReadCollectorXML(final RunData data) throws AozanException {
+      // Collect read info
+      final int readCount = data.getInt("run.info.read.count");
+
+      for (int i = 1; i <= readCount; i++) {
+        String readInfoFile =
+            RTAOutputDirPath + "/Data/reports/Summary/read" + i + ".xml";
+
+        collectRead(data, readInfoFile);
+      }
+    }
+  }
+
+  /**
+   * This class define a Collector for several binary files from InterOp
+   * directory.
+   * @since 1.1
+   * @author Sandrine Perrin
+   */
+  class ReadCollectorBinaryFile extends ReadCollector {
+
+    private void collectRead(final RunData data, final String readInfoFilePath)
+        throws AozanException {
+
+      AbstractBinaryIteratorReader.setDirectory(readInfoFilePath);
+
+      // Collect metrics on number cluster
+      new TileMetricsOutReader().collect(data);
+      // Collect metrics on error rate
+      new ErrorMetricsOutReader().collect(data);
+      // Collect metrics on intensity rate
+      new ExtractionMetricsOutReader().collect(data);
+    }
+
+    /**
+     * Public constructor
+     * @param data
+     */
+    ReadCollectorBinaryFile(final RunData data) throws AozanException {
+      System.out.println("rta " + RTAOutputDirPath);
+
+      collectRead(data, RTAOutputDirPath + "/InterOp/");
+    }
   }
 }
