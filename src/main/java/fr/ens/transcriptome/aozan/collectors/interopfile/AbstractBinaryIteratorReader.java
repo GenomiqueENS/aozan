ea2df0ad3e1d078a55bf2c4f89109315f3783096
Sandrine Perrin <sperrin@biologie.ens.fr>
2013-05-28 14:48:21 +0200

    ADD: internal class BinaryFileIterator, rewrited from library Picard.

--- src/main/java/fr/ens/transcriptome/aozan/collectors/interopfile/AbstractBinaryIteratorReader.java
+++ src/main/java/fr/ens/transcriptome/aozan/collectors/interopfile/AbstractBinaryIteratorReader.java
@@ -23,15 +23,17 @@
 
 package fr.ens.transcriptome.aozan.collectors.interopfile;
 
-import static net.sf.picard.illumina.parser.readers.MMapBackedIteratorFactory.getByteBufferIterator;
-
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.channels.FileChannel;
+import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-import net.sf.picard.illumina.parser.readers.BinaryFileIterator;
-import net.sf.picard.util.UnsignedTypeUtil;
 import fr.ens.transcriptome.aozan.AozanException;
+import fr.ens.transcriptome.eoulsan.util.FileUtils;
 
 /**
  * This class define an iterator on Illumina Metrics for reading binary files
@@ -45,12 +47,12 @@ public abstract class AbstractBinaryIteratorReader implements
   public static String dirInterOp;
 
   // 2 bytes: 1 for file version number and 1 for length for each record
-  protected final int HEADER_SIZE = 2;
+  protected static final int HEADER_SIZE = 2;
 
   protected int expectedRecordSize;
   protected int expectedVersion;
 
-  protected final BinaryFileIterator<ByteBuffer> bbIterator;
+  protected final BinaryFileIterator bbIterator;
 
   abstract public String getName();
 
@@ -84,6 +86,36 @@ public abstract class AbstractBinaryIteratorReader implements
     throw new UnsupportedOperationException();
   }
 
+  private BinaryFileIterator getByteBufferIterator(
+      final int expectedRecordSize, final File metricsOutFile)
+      throws AozanException {
+
+    final ByteBuffer buf;
+    final byte[] header = new byte[HEADER_SIZE];
+
+    try {
+      FileUtils.checkExistingFile(metricsOutFile, "");
+
+      final FileInputStream is = new FileInputStream(metricsOutFile);
+      final FileChannel channel = is.getChannel();
+      final long fileSize = channel.size();
+
+      buf = channel.map(FileChannel.MapMode.READ_ONLY, 0, fileSize);
+      buf.order(ByteOrder.LITTLE_ENDIAN);
+      channel.close();
+      is.close();
+
+    } catch (IOException e) {
+      throw new AozanException(e.getMessage());
+    }
+
+    if (HEADER_SIZE > 0)
+      buf.get(header);
+
+    return new BinaryFileIterator(header, metricsOutFile, expectedRecordSize,
+        buf);
+  }
+
   //
   // Constructor
   //
@@ -104,8 +136,7 @@ public abstract class AbstractBinaryIteratorReader implements
     this.expectedRecordSize = expectedRecordSize;
     this.expectedVersion = expectedVersion;
 
-    this.bbIterator =
-        getByteBufferIterator(HEADER_SIZE, expectedRecordSize, metricsOutFile);
+    this.bbIterator = getByteBufferIterator(expectedRecordSize, metricsOutFile);
 
     final ByteBuffer header = bbIterator.getHeaderBytes();
 
@@ -131,6 +162,100 @@ public abstract class AbstractBinaryIteratorReader implements
   //
 
   /**
+   * Copy class from Picard
+   * @author Sandrine Perrin
+   * @since 1.1
+   */
+  class BinaryFileIterator implements Iterator<ByteBuffer>,
+      Iterable<ByteBuffer> {
+
+    protected final File file;
+    protected final long fileSize;
+    protected final int elementSize;
+    private final byte[] header;
+    private final ByteBuffer buffer;
+    private byte[] localBacking;
+    private ByteBuffer localBuffer;
+
+    public ByteBuffer getHeaderBytes() {
+      final ByteBuffer bb = ByteBuffer.allocate(header.length);
+
+      bb.order(ByteOrder.LITTLE_ENDIAN);
+      bb.put(header);
+      bb.position(0);
+
+      return bb;
+    }
+
+    @Override
+    public ByteBuffer next() {
+      if (!hasNext())
+        throw new NoSuchElementException();
+
+      return getElement();
+    }
+
+    @Override
+    public Iterator<ByteBuffer> iterator() {
+      return this;
+    }
+
+    public boolean hasNext() {
+      return buffer.limit() - buffer.position() >= elementSize;
+    }
+
+    public ByteBuffer getElement() {
+      this.localBuffer.position(0);
+      this.buffer.get(this.localBacking);
+      this.localBuffer.position(0);
+
+      return localBuffer;
+    }
+
+    @Override
+    public void remove() {
+      throw new UnsupportedOperationException();
+    }
+
+    //
+    // Getter
+    //
+
+    public int getElementSize() {
+      return this.elementSize;
+    }
+
+    public File getFile() {
+      return this.file;
+    }
+
+    //
+    // Constuctor
+    //
+
+    /**
+     * Public contructor.
+     * @param header byte array for header
+     * @param file binary file
+     * @param elementSize size of element to read
+     * @param buffer byteBuffer to read the file
+     */
+    public BinaryFileIterator(final byte[] header, final File file,
+        final int elementSize, final ByteBuffer buffer) {
+      this.header = header;
+      this.file = file;
+      this.elementSize = elementSize;
+      this.fileSize = file.length();
+      this.buffer = buffer;
+
+      this.localBacking = new byte[elementSize];
+      this.localBuffer = ByteBuffer.wrap(localBacking);
+      this.localBuffer.order(ByteOrder.LITTLE_ENDIAN);
+    }
+
+  }
+
+  /**
    * This internal class define an object of illumina metrics with data always
    * present in records from binary file in InterOp directory.
    * @author Sandrine Perrin
@@ -146,10 +271,12 @@ public abstract class AbstractBinaryIteratorReader implements
       tileNumber = UnsignedTypeUtil.uShortToInt(bb.getShort());
     }
 
+    /** Get the number lane */
     public int getLaneNumber() {
       return laneNumber;
     }
 
+    /** Get the number tile */
     public int getTileNumber() {
       return tileNumber;
     }
