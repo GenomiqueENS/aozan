5eccad6153c0b529ecabd832cf07bb03ef4f4356
Sandrine Perrin <sperrin@biologie.ens.fr>
2013-08-30 10:21:27 +0200

    ADD: in configuration, new option for path log aozan file.

--- src/main/python/aozan.py
+++ src/main/python/aozan.py
@@ -207,7 +207,7 @@ def aozan_main(conf_file_path):
         sys.exit(0)
 
     # Init logger
-    Common.initLogger(conf['aozan.var.path'] + '/aozan.log')
+    Common.initLogger(conf['aozan.log.path'])
 
     # Check critical free space available
     hiseq_run.send_mail_if_critical_free_space_available(conf)
