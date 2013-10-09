43c3a868362e2c17b432a22f22f9ee44cab716c8
Sandrine Perrin <sperrin@biologie.ens.fr>
2013-10-09 16:54:22 +0200

    UPDATE: message in first-base-report mail, informations on run from RunInfo.xml and an estimation of run type.

--- src/main/python/first_base_report.py
+++ src/main/python/first_base_report.py
@@ -8,6 +8,8 @@ Created on 8 déc. 2011
 
 import common, hiseq_run
 import os
+from java.io import File
+from fr.ens.transcriptome.eoulsan.illumina import RunInfo
 
 def load_processed_run_ids(conf):
     """Load the list of the processed run ids.
@@ -60,6 +62,56 @@ def send_report(run_id, conf):
         conf: configuration dictionary
     """
 
+    #
+    # Retrieve features the current run in RunInfos.xml file
+    #
+    
+    hiseq_run_path = conf['hiseq.data.path'] + '/' + run_id
+    
+    run_info_path = hiseq_run_path + "/RunInfo.xml"
+    run_info = RunInfo()
+    run_info.parse(File(run_info_path))
+
+    
+    reads = run_info.getReads()
+    error_cycles_per_reads_not_indexes_count = 0
+    reads_indexed_count = 0
+    reads_not_indexed_count = 0
+    cycles_count = 0
+    cycles_per_reads_not_indexed = 0
+    
+    for read in reads:
+        cycles_count += read.getNumberCycles()
+        if read.isIndexedRead():
+            reads_indexed_count += 1
+        else:
+            reads_not_indexed_count += 1
+            if (cycles_per_reads_not_indexed == 0):
+                cycles_per_reads_not_indexed = read.getNumberCycles()
+            
+            # Check same cycles count for each reads not indexed
+            error_cycles_per_reads_not_indexes_count = cycles_per_reads_not_indexed != read.getNumberCycles()
+                
+    
+    # Identification type run according to data in RunInfos.xml : SR or PE
+    if reads_not_indexed_count == 1:
+        type_run_estimated = "SR-" + str(cycles_per_reads_not_indexed - 1) + " with " + str(reads_indexed_count) + " index(es)"
+    elif reads_not_indexed_count == 2:
+        type_run_estimated = "PE-" + str(cycles_per_reads_not_indexed - 1) + " with " + str(reads_indexed_count) + " index(es)"
+    else :
+        type_run_estimated = "Undetermined run type (" + str(reads_not_indexed_count) + " reads with " + str(reads_indexed_count) + " index(es))" 
+    
+    description_run = "Informations about this run :\n"
+    description_run += "\t- " + str(run_info.getFlowCellLaneCount()) + " lanes with " + str(run_info.alignToPhix.size()) + " aligned to Phix.\n"
+    description_run += "\t- " + str(reads_not_indexed_count) + " read(s) and " + str(reads_indexed_count) + " index(es).\n"
+
+    if error_cycles_per_reads_not_indexes_count or cycles_per_reads_not_indexed == 0:
+        description_run += "\t- ERROR : cycles count per reads different between reads (" + str(cycles_count) + " total cycles).\n"
+    else:
+        description_run += "\t- " + str(cycles_per_reads_not_indexed) + " cycles per reads (" + str(cycles_count) + " total cycles).\n"
+    
+    description_run += "\t- " + "estimated run type : " + type_run_estimated + ".\n"
+    
     attachment_file = hiseq_run.find_hiseq_run_path(run_id, conf) + '/' + run_id + '/First_Base_Report.htm'
-    message = 'You will find attached to this message the first base report for the run ' + run_id + '.'
-    common.send_msg_with_attachment('[Aozan] First base report for HiSeq run ' + run_id, message, attachment_file, conf)
+    message = 'You will find attached to this message the first base report for the run ' + run_id + '.\n\n' + description_run
+    common.send_msg_with_attachment('[Aozan] First base report for HiSeq run ' + type_run_estimated + '  ' + run_id , message, attachment_file, conf)
