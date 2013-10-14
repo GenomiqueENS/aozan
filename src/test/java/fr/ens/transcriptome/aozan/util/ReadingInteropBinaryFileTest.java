4bb7fed1b93fc70d55b2c6fbca97c84f52da9642
Sandrine Perrin <sperrin@biologie.ens.fr>
2013-10-14 15:59:08 +0200

    ADD: test junit for reading binary file InterOp and add source files needed.

--- src/test/java/fr/ens/transcriptome/aozan/util/ReadingInteropBinaryFileTest.java
+++ src/test/java/fr/ens/transcriptome/aozan/util/ReadingInteropBinaryFileTest.java
@@ -23,9 +23,10 @@
 
 package fr.ens.transcriptome.aozan.util;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
-import java.net.URISyntaxException;
 import java.util.List;
 import java.util.Properties;
 
@@ -36,181 +37,163 @@ import com.google.common.collect.Lists;
 import fr.ens.transcriptome.aozan.AozanException;
 import fr.ens.transcriptome.aozan.RunData;
 import fr.ens.transcriptome.aozan.collectors.ReadCollector;
+import fr.ens.transcriptome.aozan.collectors.RunInfoCollector;
 
 public class ReadingInteropBinaryFileTest extends TestCase {
-  static final String SR50_FILE = "./SR50.data";
-  static final String PE100_FILE = "./PE100.data";
+  static final String SR50_FILE = "SR50.data";
+  static final String PE100_FILE = "PE100.data";
 
-  // int read_number = 0;
-  // int lane_number = 0;
   final ReadCollector readCollector = new ReadCollector();
+  final RunInfoCollector runInfoCollector = new RunInfoCollector();
 
   final List<String> KEYS = Lists.newArrayList();
 
-  public void testSR50() {
-    RunData dataTest = new RunData();
-    RunData dataOriginal = new RunData();
+  final Properties props = new Properties();
+  String path;
 
-    int readCount = 2;
-    int laneCount = 2;
-
-    // Retrieve original runData
-    try {
-      dataOriginal.createRunDataFile(SR50_FILE);
-    } catch (IOException e1) {
-      // TODO Auto-generated catch block
-      e1.printStackTrace();
-    }
-
-    // Create runData from binary files
-    try {
-      readCollector.collect(dataTest);
-    } catch (AozanException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
-    String tileKey1;
-    String tileKey2;
-    String tileKey3;
-    String tileKey4;
-    String tileKey5;
-    String tileKey6;
-    String tileKey7;
-    String tileKey8;
-    String tileKey9;
-    String tileKey10;
-    String tileKey11;
-    String tileKey12;
-    String tileKey13;
-    String errorKey1;
-    String errorKey2;
-    String errorKey3;
-    String errorKey4;
-    String errorKey5;
-    String errorKey6;
-    String errorKey7;
-    String errorKey8;
-
-    String extractionKey1;
-    String extractionKey2;
-    String extractionKey3;
-    String extractionKey4;
-    String prefix;
-    for (int lane_number = 1; lane_number <= laneCount; lane_number++) {
-      for (int read_number = 1; read_number <= readCount; read_number++) {
-
-        prefix = "read" + read_number + ".lane" + lane_number;
-        // Values from TileMetricsBin.out
-        tileKey1 = prefix + ".called.cycles.max";
-        assertTrue(tileKey1 + " idem ? ",
-            dataTest.get(tileKey1).equals(dataOriginal.get(tileKey1)));
-        
-        tileKey2 = prefix + ".called.cycles.min";
-        tileKey3 = "phasing." + prefix + ".phasing";
-        tileKey4 = "phasing." + prefix + ".prephasing";
-        tileKey5 = prefix + ".clusters.pf";
-        tileKey6 = prefix + ".clusters.pf.sd";
-        tileKey7 = prefix + ".clusters.raw";
-        tileKey8 = prefix + ".clusters.raw.sd";
-        tileKey9 = prefix + ".prc.pf.clusters";
-        tileKey10 =
-            "read"
-                + read_number + ".lane" + lane_number + ".prc.pf.clusters.sd";
-        tileKey11 = prefix + ".tile.count";
-        tileKey12 = prefix + ".prc.align";
-        tileKey13 = prefix + ".prc.align.sd";
-
-        // Values from ErrorMetricsBin.out
-        errorKey1 = prefix + ".err.rate.100";
-        errorKey2 = prefix + ".err.rate.100.sd";
-        errorKey3 = prefix + ".err.rate.35";
-        errorKey4 = prefix + ".err.rate.35.sd";
-        errorKey5 = prefix + ".err.rate.75";
-        errorKey6 = prefix + ".err.rate.75.sd";
-        errorKey7 = prefix + ".err.rate.phix";
-        errorKey8 = prefix + ".err.rate.phix.sd";
-
-        // Values from ExtractionMetricsBin.out
-        extractionKey1 = prefix + ".first.cycle.int.pf";
-        extractionKey2 = prefix + ".first.cycle.int.pf.sd";
-        extractionKey3 = prefix + ".prc.intensity.after.20.cycles.pf";
-        extractionKey4 = prefix + ".prc.intensity.after.20.cycles.pf.sd";
-
-      }
-    }
+  public void testPE100() throws AozanException, IOException {
+    compareRunData("PE100", "InterOp_PE100", PE100_FILE);
   }
 
-  public void testPE100() {
+  public void testSR50() throws AozanException, IOException {
+    compareRunData("SR50", "InterOp_SR50", SR50_FILE);
+  }
+
+  private void compareRunData(String runName, String dir, String runDataFile)
+      throws AozanException, IOException {
+
+    // Define path to file utils
+    props.put("rta.output.dir", path + "/" + dir);
+    props.put("casava.output.dir", path + "/" + dir);
+
+    // Path to save rundata
+    runInfoCollector.configure(props);
+    readCollector.configure(props);
+
+    // Create runData from binary files
+    RunData dataTest = new RunData();
+    runInfoCollector.collect(dataTest);
+    readCollector.collect(dataTest);
+
+    // Read source rundata
+    BufferedReader br = null;
+    br =
+        new BufferedReader(new FileReader(new File(path + "/" + dir,
+            runDataFile)));
+    String line = "";
+
+    while ((line = br.readLine()) != null) {
+
+      final int pos = line.indexOf('=');
+      if (pos == -1)
+        continue;
+
+      final String key = line.substring(0, pos);
+      final String value = line.substring(pos + 1);
+
+      // Compare runData test and original for each line
+      assertTrue("For " + runName + " : " + key + " must be same ? ",
+          value.equals(dataTest.get(key)));
+
+    }
+    br.close();
   }
 
   public void setUp() {
-    Properties props = new Properties();
+
     // Reading binary files
     props.put(ReadCollector.READ_XML_COLLECTOR_SPECIFIED, "false");
     // Constant
     props.put("qc.conf.cluster.density.ratio", "0.3472222");
     // Path to directory InterOP
-    String path = "/home/sperrin/workspace/aozan/src/test/java/files";
-    // new File(
-    // fr.ens.transcriptome.aozan.util.ReadingInteropBinaryFileTest.class
-    // .getCanonicalName()).getAbsolutePath();
-
-    props.put("rta.output.dir", path);
-    props.put("casava.output.dir", path);
-
-    // Path to save rundata
-
-    readCollector.configure(props);
-
-    // // Values from TileMetricsBin.out
-    // KEYS.add("read"
-    // + read_number + ".lane" + lane_number + ".called.cycles.max");
-    // KEYS.add("read"
-    // + read_number + ".lane" + lane_number + ".called.cycles.min");
-    // KEYS.add("phasing.read" + read_number + ".lane" + lane_number +
-    // ".phasing");
-    // KEYS.add("phasing.read"
-    // + read_number + ".lane" + lane_number + ".prephasing");
-    // KEYS.add("read" + read_number + ".lane" + lane_number + ".clusters.pf");
-    // KEYS.add("read" + read_number + ".lane" + lane_number +
-    // ".clusters.pf.sd");
-    // KEYS.add("read" + read_number + ".lane" + lane_number + ".clusters.raw");
-    // KEYS.add("read" + read_number + ".lane" + lane_number +
-    // ".clusters.raw.sd");
-    // KEYS.add("read" + read_number + ".lane" + lane_number +
-    // ".prc.pf.clusters");
-    // KEYS.add("read"
-    // + read_number + ".lane" + lane_number + ".prc.pf.clusters.sd");
-    // KEYS.add("read" + read_number + ".lane" + lane_number + ".tile.count");
-    // KEYS.add("read" + read_number + ".lane" + lane_number + ".prc.align");
-    // KEYS.add("read" + read_number + ".lane" + lane_number + ".prc.align.sd");
-    //
-    // // Values from ErrorMetricsBin.out
-    // KEYS.add("read" + read_number + ".lane" + lane_number + ".err.rate.100");
-    // KEYS.add("read" + read_number + ".lane" + lane_number +
-    // ".err.rate.100.sd");
-    // KEYS.add("read" + read_number + ".lane" + lane_number + ".err.rate.35");
-    // KEYS.add("read" + read_number + ".lane" + lane_number +
-    // ".err.rate.35.sd");
-    // KEYS.add("read" + read_number + ".lane" + lane_number + ".err.rate.75");
-    // KEYS.add("read" + read_number + ".lane" + lane_number +
-    // ".err.rate.75.sd");
-    // KEYS.add("read" + read_number + ".lane" + lane_number +
-    // ".err.rate.phix");
-    // KEYS.add("read" + read_number + ".lane" + lane_number +
-    // ".err.rate.phix.sd");
-    //
-    // // Values from ExtractionMetricsBin.out
-    // KEYS.add("read"
-    // + read_number + ".lane" + lane_number + ".first.cycle.int.pf");
-    // KEYS.add("read"
-    // + read_number + ".lane" + lane_number + ".first.cycle.int.pf.sd");
-    // KEYS.add("read"
-    // + read_number + ".lane" + lane_number
-    // + ".prc.intensity.after.20.cycles.pf");
-    // KEYS.add("read"
-    // + read_number + ".lane" + lane_number
-    // + ".prc.intensity.after.20.cycles.pf.sd");
+    path =
+        new File(new File(".").getAbsolutePath() + "/src/test/java/files")
+            .getAbsolutePath();
 
   }
+
+  // private void test_to_remove() {
+  //
+  // RunData dataTest = new RunData();
+  // RunData dataOriginal = new RunData();
+  //
+  // int readCount = 2;
+  // int laneCount = 2;
+  // String tileKey1;
+  // String tileKey2;
+  // String tileKey3;
+  // String tileKey4;
+  // String tileKey5;
+  // String tileKey6;
+  // String tileKey7;
+  // String tileKey8;
+  // String tileKey9;
+  // String tileKey10;
+  // String tileKey11;
+  // String tileKey12;
+  // String tileKey13;
+  // String errorKey1;
+  // String errorKey2;
+  // String errorKey3;
+  // String errorKey4;
+  // String errorKey5;
+  // String errorKey6;
+  // String errorKey7;
+  // String errorKey8;
+  //
+  // String extractionKey1;
+  // String extractionKey2;
+  // String extractionKey3;
+  // String extractionKey4;
+  // String prefix;
+  //
+  // for (int lane_number = 1; lane_number <= laneCount; lane_number++) {
+  // for (int read_number = 1; read_number <= readCount; read_number++) {
+  //
+  // prefix = "read" + read_number + ".lane" + lane_number;
+  // // Values from TileMetricsBin.out
+  // tileKey1 = prefix + ".called.cycles.max";
+  //
+  // System.out.println(tileKey1
+  // + " " + dataTest.get(tileKey1) + " vs "
+  // + dataOriginal.get(tileKey1));
+  //
+  // assertTrue(tileKey1 + " idem ? ",
+  // dataTest.get(tileKey1).equals(dataOriginal.get(tileKey1)));
+  //
+  // tileKey2 = prefix + ".called.cycles.min";
+  // tileKey3 = "phasing." + prefix + ".phasing";
+  // tileKey4 = "phasing." + prefix + ".prephasing";
+  // tileKey5 = prefix + ".clusters.pf";
+  // tileKey6 = prefix + ".clusters.pf.sd";
+  // tileKey7 = prefix + ".clusters.raw";
+  // tileKey8 = prefix + ".clusters.raw.sd";
+  // tileKey9 = prefix + ".prc.pf.clusters";
+  // tileKey10 =
+  // "read"
+  // + read_number + ".lane" + lane_number + ".prc.pf.clusters.sd";
+  // tileKey11 = prefix + ".tile.count";
+  // tileKey12 = prefix + ".prc.align";
+  // tileKey13 = prefix + ".prc.align.sd";
+  //
+  // // Values from ErrorMetricsBin.out
+  // errorKey1 = prefix + ".err.rate.100";
+  // errorKey2 = prefix + ".err.rate.100.sd";
+  // errorKey3 = prefix + ".err.rate.35";
+  // errorKey4 = prefix + ".err.rate.35.sd";
+  // errorKey5 = prefix + ".err.rate.75";
+  // errorKey6 = prefix + ".err.rate.75.sd";
+  // errorKey7 = prefix + ".err.rate.phix";
+  // errorKey8 = prefix + ".err.rate.phix.sd";
+  //
+  // // Values from ExtractionMetricsBin.out
+  // extractionKey1 = prefix + ".first.cycle.int.pf";
+  // extractionKey2 = prefix + ".first.cycle.int.pf.sd";
+  // extractionKey3 = prefix + ".prc.intensity.after.20.cycles.pf";
+  // extractionKey4 = prefix + ".prc.intensity.after.20.cycles.pf.sd";
+  //
+  // }
+  // }
+  // }
+
 }
