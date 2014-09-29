240c3a8f413c6e276c299b2378c4db0999df3731
Sandrine Perrin <sperrin@biologie.ens.fr>
2014-09-29 17:47:06 +0200

    UPDATE: set version to add class at 1.3. ADD: header class file FIX: for BaseCountGlobalTest and NonIndexedBaseCountGlobalTest return result with double instead long.

--- src/main/java/fr/ens/transcriptome/aozan/tests/global/AbstractGlobalTest.java
+++ src/main/java/fr/ens/transcriptome/aozan/tests/global/AbstractGlobalTest.java
@@ -1,9 +1,31 @@
-package fr.ens.transcriptome.aozan.tests.global;
+/*
+ *                  Aozan development code
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU General Public License version 3 or later 
+ * and CeCILL. This should be distributed with the code. If you 
+ * do not have a copy, see:
+ *
+ *      http://www.gnu.org/licenses/gpl-3.0-standalone.html
+ *      http://www.cecill.info/licences/Licence_CeCILL_V2-en.html
+ *
+ * Copyright for this code is held jointly by the Genomic platform
+ * of the Institut de Biologie de l'École Normale Supérieure and
+ * the individual authors. These should be listed in @author doc
+ * comments.
+ *
+ * For more information on the Aozan project and its aims,
+ * or to join the Aozan Google group, visit the home page at:
+ *
+ *      http://www.transcriptome.ens.fr/aozan
+ *
+ */
 
+package fr.ens.transcriptome.aozan.tests.global;
 
 /**
  * This class define a abstract Global test.
- * @since 1.x
+ * @since 1.3
  * @author Laurent Jourdren
  */
 public abstract class AbstractGlobalTest implements GlobalTest {
