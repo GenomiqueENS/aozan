3f5864cc30422de4ed022f1fc8ac23bca983ac5a
Sandrine Perrin <sperrin@biologie.ens.fr>
2014-09-30 15:22:33 +0200

    ADD: now create a TestResult for indetermined indices samples to link report fastqscreen.

--- src/main/java/fr/ens/transcriptome/aozan/tests/sample/LinkReportFastqScreenSimpleSampleTest.java
+++ src/main/java/fr/ens/transcriptome/aozan/tests/sample/LinkReportFastqScreenSimpleSampleTest.java
@@ -47,11 +47,22 @@ public class LinkReportFastqScreenSimpleSampleTest extends AbstractSampleTest {
   public TestResult test(final RunData data, final int read,
       final int readSample, final int lane, final String sampleName) {
 
-    if (sampleName == null)
-      return new TestResult("NA");
+    if (sampleName == null) {
+      // return new TestResult("NA");
+      final String projectName = "Undetermined_indices";
 
+      final String filename =
+          String.format("lane%s_Undetermined_L%03d_R%d_001-fastqscreen.html", lane, lane,
+              readSample);
+
+      final String url = projectName + "/" + filename;
+
+      // Set score test at -1
+      return new TestResult(-1, url, "url");
+    }
+    
     // Check fastqscreen launch for sample
-    String key =
+    final String key =
         "fastqscreen.lane"
             + lane + ".sample." + sampleName + ".read" + readSample + "."
             + sampleName + ".mappedexceptgenomesample";
