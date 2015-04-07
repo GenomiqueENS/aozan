aacdd520a101d5ab19e3f820c0ae5c6fd1536086
Sandrine Perrin <sperrin@biologie.ens.fr>
2015-04-07 15:35:48 +0200

    ADD: import package illumina from Eoulsan 1.2.2, update header and import in classes.

---
+++ src/main/java/fr/ens/transcriptome/aozan/illumina/io/CasavaDesignReader.java
@@ -0,0 +1,43 @@
+/*
+ *                  Aozan development code
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU General Public License version 3 or later 
+ * and CeCILL. This should be distributed with the code. If you 
+ * do not have a copy, see:
+ *
+ *      http://www.gnu.org/licenses/gpl-3.0-standalone.html
+ *      http://www.cecill.info/licences/Licence_CeCILL_V2-en.html
+ *
+ * Copyright for this code is held jointly by the Genomic platform
+ * of the Institut de Biologie de l'École Normale Supérieure and
+ * the individual authors. These should be listed in @author doc
+ * comments.
+ *
+ * For more information on the Aozan project and its aims,
+ * or to join the Aozan Google group, visit the home page at:
+ *
+ *      http://www.transcriptome.ens.fr/aozan
+ *
+ */
+
+package fr.ens.transcriptome.aozan.illumina.io;
+
+import java.io.IOException;
+
+import fr.ens.transcriptome.aozan.illumina.CasavaDesign;
+
+/**
+ * This interface define a reader for Casava designs.
+ * @since 1.1
+ * @author Laurent Jourdren
+ */
+public interface CasavaDesignReader {
+
+  /**
+   * Read a design.
+   * @return a CasavaDesign object
+   * @throws IOException if an error occurs while reading the design
+   */
+  CasavaDesign read() throws IOException;
+}
