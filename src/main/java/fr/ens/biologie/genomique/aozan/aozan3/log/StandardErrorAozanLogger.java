1d8a78b6920a608de250827fe1121c62b9cd94d4
Laurent Jourdren <jourdren@biologie.ens.fr>
2020-08-21 17:52:49 +0200

    UPDATE: In StandardErrorAozanLogger, now flush after each log.

--- src/main/java/fr/ens/biologie/genomique/aozan/aozan3/log/StandardErrorAozanLogger.java
+++ src/main/java/fr/ens/biologie/genomique/aozan/aozan3/log/StandardErrorAozanLogger.java
@@ -1,6 +1,7 @@
 package fr.ens.biologie.genomique.aozan.aozan3.log;
 
 import java.util.logging.Handler;
+import java.util.logging.LogRecord;
 import java.util.logging.SimpleFormatter;
 import java.util.logging.StreamHandler;
 
@@ -17,7 +18,16 @@ public class StandardErrorAozanLogger extends AbstractAzoanLogger {
   @Override
   protected Handler createHandler(Configuration conf) throws Aozan3Exception {
 
-    return new StreamHandler(System.err, new SimpleFormatter());
+    return new StreamHandler(System.err, new SimpleFormatter()) {
+
+      // Force flush after each log
+      @Override
+      public synchronized void publish(final LogRecord record) {
+        super.publish(record);
+        flush();
+      }
+
+    };
   }
 
   //
